826738f7fd97c90319c4759a8331668856be3daf
Felipe Marques <felipe_marquesjr@hotmail.com>
2020-04-07 18:12:29 -0300

    Validacao sintatica com BeanValidation

--- src/main/java/com/felipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipe/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felipe.cursomc.domain.Categoria;
+import com.felipe.cursomc.dto.CategoriaDTO;
 import com.felipe.cursomc.repositories.CategoriaRepository;
 import com.felipe.cursomc.servvices.exceptions.DataIntegrityException;
 import com.felipe.cursomc.servvices.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction ){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
