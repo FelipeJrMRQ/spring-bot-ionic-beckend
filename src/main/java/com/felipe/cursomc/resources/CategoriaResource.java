6a39daa0a1f725d0941b308c5aacb7b75cfe2cd7
Felipe Marques <felipe_marquesjr@hotmail.com>
2020-04-20 20:41:50 -0300

    Busca de produtos por nome e categoria

--- src/main/java/com/felipe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipe/cursomc/resources/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,7 @@ public class CategoriaResource {
 	
 	
 	@CrossOrigin
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	@GetMapping(value="/{id}")
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
---
+++ src/main/java/com/felipe/cursomc/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.felipe.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.felipe.cursomc.domain.Categoria;
+import com.felipe.cursomc.domain.Produto;
+import com.felipe.cursomc.repositories.CategoriaRepository;
+import com.felipe.cursomc.repositories.ProdutoRepository;
+import com.felipe.cursomc.servvices.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repository;
+
+	@Autowired 
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado id: " +id+ " , Tipo: "+Produto.class.getName()));
+	}
+	
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction ){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repository.findDistinctByNomeContaining(nome, categorias, pageRequest);
+	}
+}
